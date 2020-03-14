04e73d2c8a5739bf46e94e7cb1c9c33c9f01d50a
rand0mdev <bechiirr71@gmail.com>
2020-03-14 15:03:52 +0000

    Reset migrations

--- src/Migrations/Version20200314150237.php
+++ src/Migrations/Version20200314150237.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200313225740 extends AbstractMigration
+final class Version20200314150237 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,10 @@ final class Version20200313225740 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
         $this->addSql('ALTER TABLE translate CHANGE is_suggestion is_suggestion TINYINT(1) DEFAULT NULL');
-        $this->addSql('ALTER TABLE suggestion CHANGE accepted_at accepted_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+        $this->addSql('ALTER TABLE suggestion CHANGE accepted_at accepted_at DATETIME DEFAULT NULL, CHANGE status status VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE history CHANGE is_memorised is_memorised TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -32,7 +33,8 @@ final class Version20200313225740 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE suggestion CHANGE accepted_at accepted_at DATETIME DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE history CHANGE is_memorised is_memorised TINYINT(1) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE suggestion CHANGE accepted_at accepted_at DATETIME DEFAULT \'NULL\', CHANGE status status VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE translate CHANGE is_suggestion is_suggestion TINYINT(1) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
     }
