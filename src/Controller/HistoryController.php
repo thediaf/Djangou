d5f5540eb02dc6492153e0e8f0661424037db60d
rand0mdev <bechiirr71@gmail.com>
2020-03-14 10:12:55 +0000

    added history listing

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -3,48 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\History;
-use App\Form\HistoryType;
 use App\Repository\HistoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/history")
+ * @IsGranted("ROLE_USER")
  */
 class HistoryController extends AbstractController
 {
     /**
      * @Route("/", name="history_index", methods={"GET"})
      */
-    public function index(HistoryRepository $historyRepository): Response
+    public function index(HistoryRepository $historyRepository, UserInterface $user): Response
     {
         return $this->render('history/index.html.twig', [
-            'histories' => $historyRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="history_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $history = new History();
-        $form = $this->createForm(HistoryType::class, $history);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($history);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('history_index');
-        }
-
-        return $this->render('history/new.html.twig', [
-            'history' => $history,
-            'form' => $form->createView(),
+            'histories' => $historyRepository->findBy(['user' => $user]),
         ]);
     }
 
@@ -55,26 +34,6 @@ class HistoryController extends AbstractController
     {
         return $this->render('history/show.html.twig', [
             'history' => $history,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="history_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, History $history): Response
-    {
-        $form = $this->createForm(HistoryType::class, $history);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('history_index');
-        }
-
-        return $this->render('history/edit.html.twig', [
-            'history' => $history,
-            'form' => $form->createView(),
         ]);
     }
 
--- src/Entity/Language.php
+++ src/Entity/Language.php
@@ -37,4 +37,9 @@ class Language
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
